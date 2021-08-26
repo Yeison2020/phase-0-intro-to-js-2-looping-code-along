e0ca0e8643f1b7284eecb1e908cc0036877d464f
Yeison <enmanuelquiterios@gmail.com>
2021-08-26 19:46:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+function writeCards(arr){
+    const thank = []
+    for(let i =0; i < arr.length; i++){
+
+        thank.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+
+    }
+
+    return thank;
+}
+
+
+
+
+function countDown(n){
+let countter = n;
+
+while(countter >= 0){
+  console.log(countter)
+  countter--;
+}
+}
+
+countDown(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
